f93873da6c1f655b3feacef0342296a8a0dbbbe3
Pawel Gontarz <gontarzpawe@gmail.com>
2022-03-24 13:46:54 +0100

    Add some breadcrumb styling using bootstrap

--- books/urls.py
+++ books/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import book_list, book_detail, chapter_detail
+from .views import book_list, book_detail, chapter_detail, exercise_detail
 
 app_name = 'books'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', book_list, name='book-list'),
     path('<slug:slug>/', book_detail, name='book-detail'),
     path('<book_slug>/<chapter_number>/', chapter_detail, name='chapter-detail'),
+    path('<book_slug>/<chapter_number>/<exercise_number>/', exercise_detail, name='exercise-detail'),
 ]
