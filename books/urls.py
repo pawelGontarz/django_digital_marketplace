b5c5faab849d90f3ca6c932512cbe7ff028ea836
Pawel Gontarz <gontarzpawe@gmail.com>
2022-03-24 12:41:46 +0100

    Add chapter detail view with html template

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import book_list
+from .views import book_list, book_detail, chapter_detail
 
 app_name = 'books'
 
 urlpatterns = [
-    path('', book_list, name='book-list')
+    path('', book_list, name='book-list'),
+    path('<slug:slug>/', book_detail, name='book-detail'),
+    path('<book_slug>/<chapter_number>/', chapter_detail, name='chapter-detail'),
 ]
